2beee356f933c8b7514997a047837bbd95e3d9e7
Wahid Khan <wahidzk0091@gmail.com>
2023-02-17 19:07:12 +0000

    begonia: Drop PRIVATE_BUILD_DESC
    * Set by source: https://github.com/DerpFest-AOSP/build/blob/664e18990e61c8e8279aad5e346f025716f4d712/core/sysprop.mk#L269 and https://github.com/DerpFest-AOSP/build/blob/664e18990e61c8e8279aad5e346f025716f4d712/core/sysprop.mk#L210
    Co-authored-by: MrErenK <akbaseren4751@gmail.com>

--- derp_begonia.mk
+++ derp_begonia.mk
@@ -58,7 +58,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := "Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.8.0.RGGMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.8.0.RGGMIXM release-keys" \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
 
